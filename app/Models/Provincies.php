08d6151d33c7d3d5f0b6ca3f420024dbaf27a757
Eric <efernandezl37@cepnet.net>
2021-04-08 13:54:16 +0200

    Relaciones de provincias y comarques y arreglo en botones

--- app/Models/Provincies.php
+++ app/Models/Provincies.php
@@ -11,4 +11,9 @@ class Provincies extends Model
 
     protected $table = 'provincies';
     public $timestamps = false;
+
+    public function comarques()
+    {
+        return $this->hasMany(provincies::class,"provincies_id");
+    }
 }
