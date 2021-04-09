0ec8fc2af691f83806c18f545d64b92166c11595
Eric <efernandezl37@cepnet.net>
2021-04-09 13:44:53 +0200

    botones administrador mejorados

--- resources/views/administrador/principal.blade.php
+++ resources/views/administrador/principal.blade.php
@@ -7,7 +7,7 @@
 @section('container')
 
 <div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
-    <a href="./usuaris"><button id="boto_administrador" type="button" class="btn btn-outline-primary ml-lg-5" ><strong>Usuaris i rols</strong></button></a>
+    <a href="./usuaris"><button id="boto_administrador" type="button" class="btn btn-primary ml-lg-5" ><strong>Usuaris i rols</strong></button></a>
 </div>
 <div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
     <button id="boto_administrador" type="button" class="btn btn-outline-secondary" ><strong>Alertants</strong></button>
@@ -15,14 +15,15 @@
 <div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
     <button id="boto_administrador" type="button" class="btn btn-outline-warning mr-lg-5" ><strong>Recursos</strong></button>
 </div>
-<div id="botodiv_administrador" class="float-sm-left col-md-6 col-sm-12">
-    <button id="boto_administrador" type="button" class="btn btn-outline-danger" ><strong>Helpbox</strong></button>
+<div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
+    <button id="boto_administrador" type="button" class="btn btn-outline-danger ml-lg-5" ><strong>Helpbox</strong></button>
 </div>
-<div id="botodiv_administrador" class="float-sm-right col-md-6 col-sm-12">
+<div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
     <button id="boto_administrador" type="button" class="btn btn-outline-info" ><strong>Incidències</strong></button>
 </div>
-<div id="botodiv_administrador" class="float-sm-right col-md-6 col-sm-12">
-    <button id="boto_administrador" type="button" class="btn btn-outline-dark" ><strong>Estadistiques</strong></button>
+<div id="botodiv_administrador" class="float-sm-left col-md-4 col-sm-12">
+    <button id="boto_administrador" type="button" class="btn btn-outline-dark mr-lg-5" ><strong>Estadistiques</strong></button>
+    
 </div>
 
 @endsection
