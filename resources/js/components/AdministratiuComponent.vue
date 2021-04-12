2912a95e55abf625d90608654fe685046dc2e615
Adria Plazuelo <aplazuelob73@cepnet.net>
2021-04-12 18:36:58 +0200

    Componentes administrador/administratiu realizados

--- resources/js/components/AdministratiuComponent.vue
+++ resources/js/components/AdministratiuComponent.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Example Component</div>
-
-                    <div class="card-body">
-                        I'm an example component.
-                    </div>
-                </div>
-            </div>
+    <div>
+        <div :id="div_admin_recurs" class="float-sm-left col-md-6 col-sm-12">
+            <button :id="boto_admin_recurs_for" type="button" class="btn btn-outline-success float-md-right mr-sm-1 mr-md-3 mr-lg-5" ><strong><h4>Activar Formació</h4></strong></button>
+        </div>
+        <div :id="div_admin_recurs" class="float-sm-right col-md-6 col-sm-12">
+            <a href="./trucada"><button :id="boto_admin_recurs_tru" type="button" class="btn btn-outline-warning float-md-left ml-sm-1 ml-md-3 ml-lg-5" ><strong><h4>Gestionar Trucades</h4></strong></button></a>
         </div>
     </div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                div_admin_recurs : 'botodiv_admin_recurs',
+                boto_admin_recurs_for : 'boto_admin_recurs_formacio',
+                boto_admin_recurs_tru : 'boto_admin_recurs_trucada'
+            }
+        },
         mounted() {
             console.log('Component mounted.')
         }
--- resources/views/administratiu/principal.blade.php
+++ resources/views/administratiu/principal.blade.php
@@ -5,11 +5,7 @@
     <a class="dropdown-item" href="./incidencies">Consultar incidencies</a>
 @endsection
 @section('container')
-<div id="botodiv_admin_recurs" class="float-sm-left col-md-6 col-sm-12">
-    <button id="boto_admin_recurs" type="button" class="btn btn-outline-success float-md-right mr-sm-1 mr-md-3 mr-lg-5" ><strong><h4>Activar Formació</h4></strong></button>
-</div>
-<div id="botodiv_admin_recurs" class="float-sm-right col-md-6 col-sm-12">
-    <a href="./trucada"><button id="boto_admin_recurs" type="button" class="btn btn-outline-warning float-md-left ml-sm-1 ml-md-3 ml-lg-5" ><strong><h4>Gestionar Trucades</h4></strong></button></a>
-</div>
+
+<administratiu-principal></administratiu-principal>
 
 @endsection
